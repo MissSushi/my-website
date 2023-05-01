532ef6bd9f2c55ce95a3d444995c181aac456e44
Melanie Six <melanie-six@hotmail.com>
2023-05-01 12:00:29 +0200

    Migrate code to typescript and added tailwindcss

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="md:mx-20 lg:mx-80 mx-8 my-8">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
